d76496f3eb0a9133fde9f8718855f89c1cbe082a
Alex <68423461+ReikTorrek@users.noreply.github.com>
2023-02-22 16:59:04 +0300

    Rework test task logic to jq processing

--- pages/front.php
+++ pages/front.php
@@ -7,13 +7,11 @@
         src="https://code.jquery.com/jquery-3.6.3.js"
         integrity="sha256-nQLuAZGRRcILA+6dMBOvcRh5Pe310sBpanc6+QBmyVM="
         crossorigin="anonymous"></script>
+    <script src="../scripts/front.js"></script>
 </head>
 <body>
-<h1>8-800-<?=@$_GET['num'] ?: 'DIGITS' ?></h1>
+<h1>8-800-DIGITS</h1>
 <a href="../index.html">На главную</a>
-<?php
-echo '<p> Тут какие - нить ещё штуки на странице. </p>';
-?>
-<h1>8-800-<?=@$_GET['num'] ?: 'DIGITS' ?></h1>
+<h1>8-800-DIGITS</h1>
 </body>
 </html>
---
+++ scripts/front.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    let num = getUrlParameter('num');
+    let phone = $('h1');
+    
+    phone.each(function () {
+        let phoneVal = $(this).text();
+        phone.text(phoneVal.replaceAll('DIGITS', num))
+    })
+})
+
+function getUrlParameter(sParam) {
+    let sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+}
